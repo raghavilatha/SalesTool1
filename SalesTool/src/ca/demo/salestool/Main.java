25e7826987cd148ec25a3f59696f62ff8e4c660f
raghavi <raghavilathareddy@gmail.com>
2017-04-08 22:23:04 -0700

    first change simple output

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -11,5 +11,7 @@ public static void main(String[] args) {
 private static void displayGreeting(){
 	System.out.println("HELLO HAPPY SALES PEOPLE");
 	System.out.println("THIS APP SHOWS SALES DATA");
+	
+	System.out.println("Test 1");
 }
 }
